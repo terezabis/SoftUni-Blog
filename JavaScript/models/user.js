e1a5589d878371d802f480902ed6504677da3755
Tereza Biserina <tbiserina@gmail.com>
2018-03-29 19:40:57 +0300

    initial code

---
+++ JavaScript/models/user.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+
+module.exports = function (sequelize, DataTypes) {
+    const User = sequelize.define('User', {
+        email: {
+            type: Sequelize.STRING,
+            required: true,
+            unique: true,
+            allowNull: false
+        },
+        passwordHash: {
+            type: Sequelize.STRING,
+            required: true
+        },
+        fullName: {
+            type: Sequelize.STRING,
+            required: true
+        },
+        salt: {
+            type: Sequelize.STRING,
+            required: true
+        },
+    }, {
+            timestemps: false
+
+        });
+
+    return User;
+};
